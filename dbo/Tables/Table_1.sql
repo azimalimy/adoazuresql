2c818667800b03da8e765f6c2d89f14308d1047a
Azim Ali <azimali@microsoft.com>
2024-06-03 17:49:05 +0800

    new col

--- dbo/Tables/Table_1.sql
+++ dbo/Tables/Table_1.sql
@@ -2,6 +2,8 @@
     [col1] NCHAR (10) NULL,
     [col2] NCHAR (10) NULL,
     [col3] NCHAR (2) NULL,
-    [col4] NCHAR (1) NULL
+    [col4] NCHAR (1) NULL, 
+    [col5] NCHAR(10) NULL,
+    [col6] NCHAR (2) null
 );
 
