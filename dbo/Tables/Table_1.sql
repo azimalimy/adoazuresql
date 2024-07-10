e79dac3303a219d8299868d5ffa45f50621836cf
azimalimy <azimali@microsoft.com>
2024-07-10 10:48:05 +0800

    only1column

--- dbo/Tables/Table_1.sql
+++ dbo/Tables/Table_1.sql
@@ -1,16 +1,4 @@
 ﻿CREATE TABLE [dbo].[Table_1] (
-    [col1] NCHAR (10) NULL,
-    [col2] NCHAR (10) NULL,
-    [col3] NCHAR (2) NULL,
-    [col4] NCHAR (1) NULL, 
-    [col5] NCHAR(10) NULL,
-    [col6] NCHAR (2) null,
-    [col7] NCHAR (2),
-    [col8] NCHAR (1),
-    [col9] NCHAR (2),
-    [col10] NCHAR (1), 
-    [col11] NCHAR(10) NULL,
-    [col12] NCHAR(10) NULL,
-    [col13] NCHAR(2)
+    [col1] NCHAR (10) NULL
 );
 
